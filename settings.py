e0b12f11d73d812b1528475ce9629eba6b1c178e
Irina <115899649+MullerIrina@users.noreply.github.com>
2023-09-26 22:49:12 +0200

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,7 @@
+
+valid_email = "vip.irinapopova85@mail.ru"
+valid_password = "159357"
+invalid_email="nosuchemail@nosuchhost.nosuchdomain"
+invalid_password = "15935"
+valid_email2 = "vip.irinapopova852@mail.ru"
+valid_password2 = "159357"
